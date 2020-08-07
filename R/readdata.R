051c5fa44c64373514b459aae74427274a969399
Lesley Gray <lel7lel7@gmail.com>
2020-08-07 13:50:34 +1000

    package template
    Template built in Rstudio 3.6.3

---
+++ R/readdata.R
@@ -0,0 +1,16 @@
+# Load data
+#
+# This script loads data files into R
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+readdata <- function(contractID, dir, pattern) {
+  # CONTRACT="ILMLEPIC-15325"
+  # TARGETS = read.metharray.sheet(base="/data/Bioinfo/bioinfo-proj-grayl/projects_methylation/CAGRF20422/", pattern=".csv$")
+  # DATA = "IDAT"
+  # EPIC = read.metharray.exp(base = DATA)
+  }
